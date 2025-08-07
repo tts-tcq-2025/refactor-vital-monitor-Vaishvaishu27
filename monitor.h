00640a8549480dff0aca6eca71248ad6b444962c
Vaishvaishu27 <chinnumunnu2762006@gmail.com>
2025-08-07 09:24:29 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,3 +1,20 @@
+
 #pragma once
 
+// Function declaration
 int vitalsOk(float temperature, float pulseRate, float spo2);
+
+// Enum to represent vital status
+enum VitalStatus {
+  OK,
+  TEMP_CRITICAL,
+  PULSE_CRITICAL,
+  SPO2_CRITICAL
+};
+
+// Pure function to check vitals
+VitalStatus checkVitals(float temperature, float pulseRate, float spo2);
+
+// Alert function for I/O
+void showAlert(const std::string& message);
+

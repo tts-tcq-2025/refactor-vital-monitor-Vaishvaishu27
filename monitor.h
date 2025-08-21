a32088a8a8cf0dbc71a28753f508c19cac797334
Vaishvaishu27 <chinnumunnu2762006@gmail.com>
2025-08-21 10:00:11 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,21 +1,6 @@
-#ifndef MONITOR_H
-#define MONITOR_H
+#pragma once
 
-#include "vitals.h"
-#include <string>
-#include <map>
-
-extern const Boundary tempBoundary;
-extern const Boundary pulseBoundary;
-extern const Boundary spo2Boundary;
-
-// Map Condition enums to messages for each vital sign
-const std::map<Condition, std::string>& getMessageMap(const std::string& vitalName);
-
-// Helper to get message string given condition and boundary for the vital
-std::string conditionToMessage(Condition condition, const std::string& vitalName);
-
-#endif
+int vitalsOk(float temperature, float pulseRate, float spo2);
 
 
 

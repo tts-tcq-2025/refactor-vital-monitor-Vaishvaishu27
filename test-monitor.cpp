a39216375c34eaef9851839ed0cfb58a6ddffa66
Vaishvaishu27 <chinnumunnu2762006@gmail.com>
2025-08-21 09:21:36 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -2,24 +2,13 @@
 #include "./monitor.h"
 
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));  // Pulse high, SpO2 low
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));  // All normal
+  ASSERT_FALSE(vitalsOk(99, 102, 70));
+  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+  ASSERT_TRUE(vitalsOk(95, 60, 90));
+  ASSERT_TRUE(vitalsOk(102, 100, 90));
+  ASSERT_FALSE(vitalsOk(94, 59, 89));
+  ASSERT_FALSE(vitalsOk(103, 101, 91));
+  ASSERT_TRUE(vitalsOk(101, 100, 90));
+  ASSERT_TRUE(vitalsOk(96, 100, 90));
 }
 
-TEST(Monitor, TemperatureOutOfRange) {
-  ASSERT_EQ(checkVitals(103, 80, 95), TEMP_CRITICAL);
-  ASSERT_EQ(checkVitals(94, 80, 95), TEMP_CRITICAL);
-}
-
-TEST(Monitor, PulseRateOutOfRange) {
-  ASSERT_EQ(checkVitals(98.6, 55, 95), PULSE_CRITICAL);
-  ASSERT_EQ(checkVitals(98.6, 105, 95), PULSE_CRITICAL);
-}
-
-TEST(Monitor, SpO2OutOfRange) {
-  ASSERT_EQ(checkVitals(98.6, 75, 85), SPO2_CRITICAL);
-}
-
-TEST(Monitor, AllVitalsNormal) {
-  ASSERT_EQ(checkVitals(98.6, 75, 95), OK);
-}
